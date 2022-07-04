4b32b65e8fada6ea6a9a4496a0331e0c15a71f18
Nosrey <106562225+Nosrey@users.noreply.github.com>
2022-07-04 16:24:58 -0400

    first homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,30 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var arr = String(num).split('').map(function(num) {
+    return Number(num)
+  })
+  var total = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      continue;
+    } 
+    total += (arr[i] * (2 ** ((arr.length - 1) - i)))
+  }
+
+  return total;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var final = [];
+  var numero = num;
+  while (numero != 0) {
+    final.unshift(numero % 2);
+    numero = Math.floor(numero/2);
+  }
+  return final.join('');
 }
 
 
