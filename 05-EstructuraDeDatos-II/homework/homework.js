54a9c6c9c756f0d33fc88c3b67fd7cf0734257aa
Nosrey <106562225+Nosrey@users.noreply.github.com>
2022-07-11 12:25:10 -0400

    hw 5 final update all passed

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -11,9 +11,86 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
 
-function LinkedList() {}
+function LinkedList() {
+  this.head = null;
+}
 
-function Node(value) {}
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+LinkedList.prototype.add = function(elemento) {
+  var node = new Node(elemento);
+  var actual = this.head;
+  if (!this.head) {
+      this.head = node;
+      return node;
+  }
+  else {
+      while (actual.next) {
+          actual = actual.next;
+      }
+      actual.next = node;
+      return node;
+  }
+}
+
+LinkedList.prototype.remove = function() {
+  var actual = this.head;
+  var borrar = null;
+  if (!actual) {
+      return null;
+  }
+  else if (!actual.next) {
+      borrar = this.head.value;
+      this.head = null;
+      return borrar;
+  }
+  else {
+      while (actual.next.next) {
+        actual = actual.next;
+      }
+      borrar = actual.next.value;
+      actual.next = null;
+      return borrar;
+  }
+}
+
+LinkedList.prototype.search = function(objetivo) {
+  var actual = this.head;
+  if (!actual) {
+    return null;
+  }
+  if (objetivo instanceof Function) {
+    if (!actual.next) {
+      if (objetivo(actual.value)) {return actual.value;}
+      else return null;
+    }
+    else {
+      while (actual.next) {
+        if (objetivo(actual.value)) {return actual.value}
+        actual = actual.next;
+      }
+      if (objetivo(actual.value)) {return actual.value}
+      else return null;
+    }
+  }
+  else {
+    if (!actual.next) {
+      if (actual.value === objetivo) {return actual.value;}
+      else return null;
+    }
+    else {
+      while (actual.next) {
+        if (actual.value === objetivo) {return actual.value;}
+        actual = actual.next;
+      }
+      if (actual.value === objetivo) {return actual.value;}
+      else return null;
+    }
+  }
+}
 
 /*
 Implementar la clase HashTable.
@@ -30,7 +107,47 @@ La clase debe tener los siguientes métodos:
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 
-function HashTable() {}
+function HashTable() {
+  this.array = [];  
+  this.numBuckets = 35;
+}
+
+HashTable.prototype.hash = function(valor) {
+  var total = 0;
+  for (var i = 0; i < valor.length; i++) {
+    total += valor[i].charCodeAt(0);
+  }
+  return (total % this.numBuckets);
+}
+
+HashTable.prototype.set = function(clave,valor) {
+  if (typeof(clave) !== 'string') {throw new TypeError('Keys must be strings');}
+
+  var hashed = this.hash(clave);
+
+  if (!this.array[hashed]) {
+    this.array[hashed] = {}
+  }
+  
+  this.array[hashed][clave] = valor;
+}
+
+HashTable.prototype.get = function(clave) {
+  var hashed = this.hash(clave);
+  return this.array[hashed][clave];
+}
+
+HashTable.prototype.hasKey = function(clave) {
+  var hashed = this.get(clave);
+  if (hashed) {
+    return true;
+  }
+  else return false;
+}
+
+var ejemplo = new HashTable();
+
+ejemplo.set('hola','pa');
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
